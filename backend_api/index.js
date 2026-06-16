715e1325c661f1f155200e5a4b6be1db8b8a3e28
Tiya Kesharwani <kesharwanitiya@gmail.com>
2026-06-16 03:10:39 -0700

    update db config

--- backend_api/index.js
+++ backend_api/index.js
@@ -8,12 +8,12 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'narcotics_user',
-    password: 'StrongPassword123',
-    database: 'cg_narcotics_trace_db'
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
 });
-
 db.connect((err) => {
     if (err) console.log("DB Connection Failed:", err);
     else console.log("Connected to MySQL Database");
